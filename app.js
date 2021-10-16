919842f2e1479f24f9bd79d74bc584b16c69af8d
marwan <marwan.alsubayi@gmail.com>
2021-10-16 15:09:19 +0300

    host

--- app.js
+++ app.js
@@ -23,12 +23,14 @@ const creatToken = (id, email) => {
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,"auth");
-//   next();
-// });
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept","auth");
+  next();
+});
+
+
 var cors = require('cors')
 app.use(cors());
 
